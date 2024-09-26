925a3c79b0c60ededfc6f1f35436a08c6ac56b1b
gitsaugat <letnotsay@gmail.com>
2024-09-26 10:12:47 -0400

    Update: api urls

--- utils/apis.js
+++ utils/apis.js
@@ -3,31 +3,36 @@ const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 const API_URLS = {
   finance: {
     unbookedTransactions: function () {
-      return BASE_URL + "/api/unbooked-transactions";
+      return BASE_URL + "/financial/unbooked-transactions/";
     },
     paymentTrackerWeekly: function () {
       return BASE_URL + "/api/payment-tracking-weekly";
     },
     clientSummary: function () {
-      return BASE_URL + "/api/client-table";
+      return BASE_URL + "/financial-overview-General-client-table/";
     },
     paymentTracking: function () {
-      return BASE_URL + "/api/payment-tracking";
+      return BASE_URL + "/client/payment-tracking/";
     },
     generalDetails: function () {
-      return BASE_URL + "/api/general-details";
+      return BASE_URL + "/financial-overview-General/";
     },
     financialTransactionPerClient: function (clientCode) {
-      return BASE_URL + `/api/financial-transaction?account_code=${clientCode}`;
+      return (
+        BASE_URL +
+        `/financial-transaction-data-perclient/?account_code=${clientCode}`
+      );
     },
     bankTransactionDetailPerClient: function (clientCode) {
       return (
-        BASE_URL + `/api/transaction-bank-details?account_code=${clientCode}`
+        BASE_URL +
+        `/transaction-bank-details-perclient/?account_code=${clientCode}`
       );
     },
     invoiceDetailPerClient: function (clientCode) {
       return (
-        BASE_URL + `/api/transaction-invoice-details?account_code=${clientCode}`
+        BASE_URL +
+        `/transaction-invoice-detail-perclient/?account_code=${clientCode}`
       );
     },
     bankAndInvoiceDetailsPerClient: function (clientCode) {
@@ -39,16 +44,16 @@ const API_URLS = {
   },
   labels: {
     listAllLabelsByGroup: () => {
-      return BASE_URL + "/api/list-labels-by-group";
+      return BASE_URL + "/api/list-all-labels-grouped-by-type/";
     },
     updateLabels: () => {
-      return BASE_URL + "/api/update-label";
+      return BASE_URL + "/api/Edit-Label-name-for-type/";
     },
     deleteLabels: () => {
-      return BASE_URL + "/api/delete-label/";
+      return BASE_URL + "/api/Delete-label-for-type/";
     },
     createLabels: () => {
-      return BASE_URL + "/api/create-label";
+      return BASE_URL + "/api/Create-New-label-for-type/";
     },
   },
 };
