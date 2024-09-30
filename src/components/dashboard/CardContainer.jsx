d0ff461e86c842411e3176c156f25663f16c569e
gitsaugat <letnotsay@gmail.com>
2024-09-29 21:27:30 -0400

    Update: container height

--- src/components/dashboard/CardContainer.jsx
+++ src/components/dashboard/CardContainer.jsx
@@ -5,7 +5,7 @@ export default function CardContainer({ header = false, children, onClick }) {
       className="divide-y  divide-gray-200 overflow-hidden rounded-lg bg-white shadow cursor-pointer"
     >
       {header && <div className="px-4 py-3 sm:px-6 bg-blue-100">{header}</div>}
-      <div className="px-4 py-3 sm:p-6 max-h-96 overflow-scroll">
+      <div className="px-4 py-3 sm:p-6 max-h-fit overflow-scroll">
         {children}
       </div>
     </div>
